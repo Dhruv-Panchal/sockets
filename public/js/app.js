c5c611c347b2aa321cf85cfd885946941b75f2b6
dhruv panchal <dhruvpanchal96@gmail.com>
2017-09-05 22:29:42 +0530

    added front end for submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,23 @@
 var socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', function () {
     console.log('Conncted to socket.io server!');
 });
 
-socket.on('message', function(message) {
+socket.on('message', function (message) {
     console.log('New message:');
     console.log(message.text);
+});
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function () {
+    event.preventDefault();
+
+    var $message = $form.find('input[name=message]');
+
+    socket.emit('message', {
+        text: $message.val()
+    });
+    $message.val('');
 });
